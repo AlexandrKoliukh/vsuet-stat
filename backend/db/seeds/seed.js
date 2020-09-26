23fc3d9544180d8a1c3337313eace8e1e7e34f43
AlexandrKoliukh <alexkoliukh@gmail.com>
2020-09-26 13:11:01 +0300

    dis seeds

--- backend/db/seeds/seed.js
+++ backend/db/seeds/seed.js
@@ -1,86 +1,86 @@
-exports.seed = (knex) =>
-  knex('subjects')
-    .del()
-    .then(() =>
-      knex('teachers')
-        .del()
-        .then(() =>
-          knex('teachers').insert([
-            {
-              id: 1,
-              first_name: '',
-              second_name: 'Коробова',
-              middle_name: '',
-            },
-            {
-              id: 2,
-              first_name: '',
-              second_name: 'Никитин',
-              middle_name: '',
-            },
-            {
-              id: 3,
-              first_name: '',
-              second_name: 'Рылёв',
-              middle_name: '',
-            },
-            {
-              id: 4,
-              first_name: '',
-              second_name: 'Сафонова',
-              middle_name: '',
-            },
-            {
-              id: 5,
-              first_name: '',
-              second_name: 'Бабаева',
-              middle_name: '',
-            },
-            {
-              id: 6,
-              first_name: '',
-              second_name: 'Великородный',
-              middle_name: '',
-            },
-          ])
-        )
-        .then(() =>
-          knex('subjects').insert([
-            {
-              id: 1,
-              name: 'СТРПО',
-              teacher_id: 3,
-            },
-            {
-              id: 2,
-              name: 'Мат. Мод.',
-              teacher_id: 1,
-            },
-            {
-              id: 3,
-              name: 'МММППР',
-              teacher_id: 2,
-            },
-            {
-              id: 4,
-              name: 'МТПИС',
-              teacher_id: 4,
-            },
-            {
-              id: 5,
-              name: 'СПИ',
-              teacher_id: 4,
-            },
-            {
-              id: 6,
-              name: 'ИОППИ',
-              teacher_id: 5,
-            },
-            {
-              id: 7,
-              name: 'ОНИД',
-              teacher_id: 6,
-            },
-          ])
-        )
-    );
+// exports.seed = (knex) =>
+//   knex('subjects')
+//     .del()
+//     .then(() =>
+//       knex('teachers')
+//         .del()
+//         .then(() =>
+//           knex('teachers').insert([
+//             {
+//               id: 1,
+//               first_name: '',
+//               second_name: 'Коробова',
+//               middle_name: '',
+//             },
+//             {
+//               id: 2,
+//               first_name: '',
+//               second_name: 'Никитин',
+//               middle_name: '',
+//             },
+//             {
+//               id: 3,
+//               first_name: '',
+//               second_name: 'Рылёв',
+//               middle_name: '',
+//             },
+//             {
+//               id: 4,
+//               first_name: '',
+//               second_name: 'Сафонова',
+//               middle_name: '',
+//             },
+//             {
+//               id: 5,
+//               first_name: '',
+//               second_name: 'Бабаева',
+//               middle_name: '',
+//             },
+//             {
+//               id: 6,
+//               first_name: '',
+//               second_name: 'Великородный',
+//               middle_name: '',
+//             },
+//           ])
+//         )
+//         .then(() =>
+//           knex('subjects').insert([
+//             {
+//               id: 1,
+//               name: 'СТРПО',
+//               teacher_id: 3,
+//             },
+//             {
+//               id: 2,
+//               name: 'Мат. Мод.',
+//               teacher_id: 1,
+//             },
+//             {
+//               id: 3,
+//               name: 'МММППР',
+//               teacher_id: 2,
+//             },
+//             {
+//               id: 4,
+//               name: 'МТПИС',
+//               teacher_id: 4,
+//             },
+//             {
+//               id: 5,
+//               name: 'СПИ',
+//               teacher_id: 4,
+//             },
+//             {
+//               id: 6,
+//               name: 'ИОППИ',
+//               teacher_id: 5,
+//             },
+//             {
+//               id: 7,
+//               name: 'ОНИД',
+//               teacher_id: 6,
+//             },
+//           ])
+//         )
+//     );
