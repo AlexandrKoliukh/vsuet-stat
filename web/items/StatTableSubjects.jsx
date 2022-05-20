555cc1c96475651025e13b5099be51e41c443a55
alk <akoliukh@gmail.com>
2022-05-20 13:48:05 +0300

    фикс заголовок

--- web/items/StatTableSubjects.jsx
+++ web/items/StatTableSubjects.jsx
@@ -3,14 +3,15 @@ import { useSelector } from 'react-redux';
 import { selectSubjectsSortedByProfileLimits } from '../store/subjectsSlice';
 
 export const StatTableSubjects = () => {
-  const a = useSelector(selectSubjectsSortedByProfileLimits);
+  const profiles = useSelector(selectSubjectsSortedByProfileLimits);
 
-  console.log(a);
+  if (profiles.length === 0) return null;
+
   return (
     <div>
       <h3>Рейтинг предметов (пороговое агрегирование)</h3>
       <ol>
-        {a.map((profile) => {
+        {profiles.map((profile) => {
           const subjectName = profile.subject.name;
 
           return <li key={profile.id}>{subjectName}</li>;
